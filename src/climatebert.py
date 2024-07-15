fa556c1a284a38a8179c03cc7a9c54b6045f1a63
stefan <arseneausm@gmail.com>
2024-07-15 13:47:24 -0400

    added a climatebert call

--- src/climatebert.py
+++ src/climatebert.py
@@ -5,20 +5,38 @@ from transformers.pipelines.pt_utils import KeyDataset
 import datasets
 from tqdm.auto import tqdm
 
+import dataloader as dl
 import argparse
-import cleandata
 
-dataset_name = "climatebert/climate_commitments_actions"
-model_name = "climatebert/distilroberta-base-climate-commitment"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', nargs='?', default=None)
+    parser.add_argument('--savefile', action='store_true')
+    args = parser.parse_args()
 
-# If you want to use your own data, simply load them as 🤗 Datasets dataset, see https://huggingface.co/docs/datasets/loading
-dataset = datasets.load_dataset(dataset_name, split="test")
+    n = int(args.n)
+    savefile = args.savefile
 
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name, max_len=512)
+    dataset_name = "climatebert/climate_commitments_actions"
+    model_name = "climatebert/distilroberta-base-climate-commitment"
 
-pipe = pipeline("text-classification", model=model, tokenizer=tokenizer, device=0)
+    # If you want to use your own data, simply load them as 🤗 Datasets dataset, see https://huggingface.co/docs/datasets/loading
+    #dataset = datasets.load_dataset(dataset_name, split="test")
+    datafr = dl.DataLoader(n, savefile)
+    dataset_hf = datafr.speechdata_hf
+    dataset_pd = datafr.speechdata_pd
 
-# See https://huggingface.co/docs/transformers/main_classes/pipelines#transformers.pipeline
-for out in tqdm(pipe(KeyDataset(dataset, "text"), padding=True, truncation=True)):
-   print(out)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, max_len=512)
+
+    pipe = pipeline("text-classification", model=model, tokenizer=tokenizer, device=0)
+
+    # See https://huggingface.co/docs/transformers/main_classes/pipelines#transformers.pipeline
+    labels, scores = [], []
+    for out in tqdm(pipe(KeyDataset(dataset_hf, "paragraph"), padding=True, truncation=True)):
+       labels.append(out['label'])
+       scores.append(out['score'])
+
+    dataset_pd['climatebert_labels'] = labels
+    dataset_pd['climatebert_scores'] = scores
+    dataset_pd.to_csv('test.csv')
