b4b499ed42398ea46b74a4ee336e80400f2d8642
stefan <arseneausm@gmail.com>
2024-08-30 17:01:46 -0400

    added gpt data

--- src/gpt.py
+++ src/gpt.py
@@ -8,8 +8,46 @@ from openai import OpenAI
 
 import dataloader as dl
 import argparse
+
+class GPTClassifier:
+    def __init__(self):
+      self.client = OpenAI()
+
+    def query(self, paragraph):
+        completion = self.client.chat.completions.create(
+                            model="gpt-3.5-turbo",
+                            messages=[
+                                {
+                                "role": "system",
+                                "content": "You will be provided with a central bank speech which may or may not be about climate change. A central bank speech about climate change typically addresses the financial and economic implications of climate change, climate risks, and sustainability. It explores how climate change can impact economic and financial market activity, the stability of the financial system, the stability of the banking system, and it might emphasize the need for monitoring and robust financial strategies and regulations to mitigate these risks. The speech may outline the central bank's role in supervision and regulation, incorporating climate-related financial disclosures, fostering green investments, and supporting policy measures that align with climate goals. By highlighting the intersection of environmental sustainability and monetary policy, the speech aims to drive awareness and action among financial institutions, policymakers, and the public, promoting a resilient and sustainable economic framework in the face of climate challenges. Respond ``yes'' if this speech is climate related and ``no'' if it is not."
+                                },
+                                {
+                                "role": "user",
+                                "content": f"{paragraph}"
+                                }
+                            ],
+                            temperature=0.7,
+                            top_p=1
+                        )
+        response = completion.choices[0].message.content.lower()
+        return response        
+
+    def __call__(self, dataset):
+        labels, scores = [], []
+        for text in dataset['paragraph']:
+            response = self.query(text)
+            labels.append(response)
+
+            if response == 'yes':
+                scores.append(1)
+            elif response == 'no':
+                scores.append(0)
+            else:
+                scores.append(999)
+
+        return labels, scores
    
-def summation_score(pd_dataset, weight):
+def summation_score(scores):
     classifier = (2*np.all([pd_dataset['label'] == 'yes'], axis = 0).astype(float) - 1)
     classifier *= pd_dataset['score'].to_numpy()
     raw_scores = classifier.copy()
@@ -22,38 +60,14 @@ def classify_speeches(ids, sentence_chunking, score_weighting):
     parameters = {}
 
     dat = dl.DataLoader()
-    client = OpenAI()
+    gpt_classifier = GPTClassifier()
 
     for i, id in enumerate(tqdm(ids)):
         url, title, description, author, date, text, dataset_pd, dataset_hf = dat.fetch_text(id, sentence_chunking = int(sentence_chunking))
-
         if dataset_hf != '':
-            completion = client.chat.completions.create(
-                            model="gpt-3.5-turbo",
-                            messages=[
-                                {
-                                "role": "system",
-                                "content": "You will be provided with a central bank speech which may or may not be about climate change. A central bank speech about climate change typically addresses the financial and economic implications of climate change, climate risks, and sustainability. It explores how climate change can impact economic and financial market activity, the stability of the financial system, the stability of the banking system, and it might emphasize the need for monitoring and robust financial strategies and regulations to mitigate these risks. The speech may outline the central bank's role in supervision and regulation, incorporating climate-related financial disclosures, fostering green investments, and supporting policy measures that align with climate goals. By highlighting the intersection of environmental sustainability and monetary policy, the speech aims to drive awareness and action among financial institutions, policymakers, and the public, promoting a resilient and sustainable economic framework in the face of climate challenges. Respond ``yes'' if this speech is climate related and ``no'' if it is not."
-                                },
-                                {
-                                "role": "user",
-                                "content": f"{text}"
-                                }
-                            ],
-                            temperature=0.7,
-                            top_p=1
-                        )
-
-            response = completion.choices[0].message.content.lower()
-            parameters[id] = response
-
-            if response == 'yes':
-                final_scores.append(1)
-            elif response == 'no':
-                final_scores.append(0)
-            else:
-                final_scores.append(999)
-
+            labels, scores = gpt_classifier(dataset_pd)
+            parameters[id] = np.array([labels, scores]).T
+            final_scores.append(np.mean(scores))            
         else:
             final_scores.append(-999)
             parameters[id] = []
